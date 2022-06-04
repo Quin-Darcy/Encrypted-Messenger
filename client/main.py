8d0b3f1066953fca09879f4cda39a69b71d89990
Quin-Darcy <quintondarcy@csus.edu>
2022-06-04 12:17:47 -0700

    client and server now take command line arguments

--- client/main.py
+++ client/main.py
@@ -1,13 +1,53 @@
 #!/usr/bin/env python3
 import client
+import sys
 
 
-PORT = 12345
-HOSTNAME = "127.0.0.1"
+def error_msg():
+    print("Usage: python3 main.py {target specification}")
+    print("TARGET SPECIFICATION:")
+    print(f"{'Ex: python3 main.py -a 127.0.0.1:12345':>40}")
+    print(f"{'-a <IP:PORT>: IP address and port number':>42}")
 
-def main():
+def parse_input(args):
+    if args[1] == '-a':
+        addr = args[2].split(':')
+        if len(addr) == 2:
+            ip = addr[0].split('.')
+            port = addr[1]
+            
+            for n in ip:
+                try:
+                    if n != str(int(n)) or int(n) < 0 or int(n) > 255:
+                        print(f"Failed to resolve '{addr[0]}'")
+                        return False
+                except:
+                    return False
+
+            try:
+                if port != str(int(port)) or int(port) < 0:
+                    return False
+            except:
+                return False
+
+            return [addr[0], int(addr[1])]
+
+        else:
+            error_msg()
+            return False
+    else:
+        error_msg()
+        return False
+
+
+def main(hostname, port):
     username = input("Username: ")
-    client.Client(HOSTNAME, PORT, username)
+    client.Client(hostname, port, username)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 3:
+        args = parse_input(sys.argv)
+        if args != False:
+            main(args[0], args[1])
+    else:
+        error_msg()
--- server/main.py
+++ server/main.py
@@ -1,14 +1,74 @@
 #!/usr/bin/env python3
 import server
+import sys
 
 
-PORT = 12345
-HOSTNAME = "127.0.0.1"
+def error_msg():
+    print("Usage: python3 main.py {target specification} [keysize]")
+    print("TARGET SPECIFICATION:")
+    print(f"{'Ex: python3 main.py -a 127.0.0.1:12345':>40}")
+    print(f"{'-a <IP:PORT>: IP address and port number':>42}")
+    print("KEYSIZE:")
+    print(f"{'Ex: python3 main.py -a 127.0.0.1:12345 -k 256':>47}")
+    print(f"{'-k <num_of_bits>: Size of common key in bits':>46}")
 
-KEYSIZE = 128
+def parse_input(args):
+    addr = []
+    key = None
+    if args[1] == '-a':
+        addr = args[2].split(':')
+        if len(addr) == 2:
+            ip = addr[0].split('.')
+            port = addr[1]
+            
+            for n in ip:
+                try:
+                    if n != str(int(n)) or int(n) < 0 or int(n) > 255:
+                        print(f"Failed to resolve '{addr[0]}'")
+                        return False
+                except:
+                    return False
 
-def main():
-    server.Server(HOSTNAME, PORT, KEYSIZE)
+            try:
+                if port != str(int(port)) or int(port) < 0:
+                    return False
+            except:
+                return False
+
+        else:
+            error_msg()
+            return False
+    else:
+        error_msg()
+        return False
+
+    if len(args) == 5:
+        if args[3] == '-k':
+            key = args[4]
+            try:
+                if key != str(int(key)) or int(key) < 0:
+                    error_msg()
+                    return False
+            except:
+                return False
+        else:
+            return False
+
+        return [addr[0], int(addr[1]), int(key)]
+    else:
+        return [addr[0], int(addr[1])]
+
+def main(hostname, port, keysize=128):
+    server.Server(hostname, port, keysize)
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) >= 3:
+        args = parse_input(sys.argv)
+        if args != False:
+            if len(args) == 3:
+                main(args[0], args[1], args[2])
+            else:
+                main(args[0], args[1])
+    else:
+        error_msg()
+
